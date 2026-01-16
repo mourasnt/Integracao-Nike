17b0dc0b7ee3abeb29c493b4a7d563e4385fb7d8
Willian <ws.prado@icloud.com>
2026-01-16 14:56:52 -0300

    migration

--- alembic/versions/0003_add_xmls_b64_to_shipment_invoices.py
+++ alembic/versions/0003_add_xmls_b64_to_shipment_invoices.py
@@ -1,7 +1,7 @@
 """add xmls_b64 column to shipment_invoices
 
 Revision ID: 0003_add_xmls_b64
-Revises: 0002_add_status
+Revises: 0001_initial
 Create Date: 2026-01-16 00:00:00.000000
 """
 from alembic import op
@@ -9,7 +9,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '0003_add_xmls_b64'
-down_revision = '0002_add_status'
+down_revision = '0001_initial'
 branch_labels = None
 depends_on = None
 
