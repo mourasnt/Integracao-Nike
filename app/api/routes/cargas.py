77b5675947c1d794022eabf431e690a8a365fdff
Willian <ws.prado@icloud.com>
2026-01-19 17:21:35 -0300

    ajuste

--- app/api/routes/cargas.py
+++ app/api/routes/cargas.py
@@ -74,7 +74,10 @@ async def obter_carga(carga_id: int,  current_user: str = Depends(get_current_us
 @router.post("/{carga_id}/status")
 async def alterar_status(
     carga_id: int,
-    novo_status: Optional[Any] = Body(None, example={"code": "1"}, description="Código do novo status (p.ex. {\"code\": \"1\"} ou ""1"""),
+    novo_status: Optional[Any] = Body(
+        None,
+        description="Código do novo status (ex: {\"code\": \"1\"} ou \"1\")",
+    ),
     anexo: Optional[UploadFile] = File(None),
     request: Request = None,
     db: AsyncSession = Depends(get_db),
