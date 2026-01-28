124fa4f8a7c3f7f4f13dbb806880f7e69dbe22d8
Willian <ws.prado@icloud.com>
2026-01-28 15:39:08 -0300

    ajuste final

--- app/core/config.py
+++ app/core/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     secret_key: str = Field(default="change-me-very-secret", env="SECRET_KEY")
     access_token_expire_minutes: int = Field(default=120, env="ACCESS_TOKEN_EXPIRE_MINUTES")
     jwt_algorithm: str = Field(default="HS256", env="JWT_ALGORITHM")
-    front_users: list[str] = Field(default=["integracao_logistica", "sbf"], env="FRONT_USERS")
+    front_users: list[str] = Field(default=["integracao_logistica", "SBF"], env="FRONT_USERS")
     front_admin_users: list[str] = Field(default=["integracao_logistica"], env="FRONT_ADMIN_USERS")
 
 settings = Settings()
